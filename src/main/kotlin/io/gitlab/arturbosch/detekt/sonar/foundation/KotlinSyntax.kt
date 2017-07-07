3cd8146bbce73655b957c9df36b8e24590b530c9
Artur Bosch <arturbosch@gmx.de>
2017-07-07 09:57:02 +0200

    Highlight string start-, end-quote and template symbols - closes #2

--- src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/foundation/KotlinSyntax.kt
+++ src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/foundation/KotlinSyntax.kt
@@ -25,13 +25,13 @@ object KotlinSyntax {
 		ktFile.node.visitTokens {
 			when (it.elementType) {
 				in KtTokens.KEYWORDS -> syntax.highlightByType(it, TypeOfText.KEYWORD)
-				in KtTokens.SOFT_KEYWORDS -> syntax.highlightByType(it, TypeOfText.KEYWORD_LIGHT)
+				in KtTokens.SOFT_KEYWORDS -> syntax.highlightByType(it, TypeOfText.KEYWORD)
 				in KtTokens.STRINGS -> syntax.highlightByType(it, TypeOfText.STRING)
 				in KtTokens.COMMENTS -> syntax.highlightByType(it, TypeOfText.COMMENT)
-				KtTokens.SHORT_TEMPLATE_ENTRY_START -> syntax.highlightByType(it, TypeOfText.STRING)
-				KtTokens.LONG_TEMPLATE_ENTRY_START -> syntax.highlightByType(it, TypeOfText.STRING)
-				KtTokens.LONG_TEMPLATE_ENTRY_END -> syntax.highlightByType(it, TypeOfText.STRING)
+				KtTokens.OPEN_QUOTE, KtTokens.CLOSING_QUOTE -> syntax.highlightByType(it, TypeOfText.STRING)
 				KtTokens.AT -> syntax.handleAnnotations(it)
+				KtTokens.SHORT_TEMPLATE_ENTRY_START, KtTokens.LONG_TEMPLATE_ENTRY_START, KtTokens.ESCAPE_SEQUENCE,
+				KtTokens.LONG_TEMPLATE_ENTRY_END -> syntax.highlightByType(it, TypeOfText.ANNOTATION)
 			}
 		}
 
