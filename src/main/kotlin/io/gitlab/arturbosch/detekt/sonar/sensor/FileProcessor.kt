c037e88b693065ead24fd99b7e514e04be096cf6
Olivier Lemasle <o.lemasle@gmail.com>
2017-12-05 21:19:53 +0100

    Update detekt and adapt to new version

--- src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/sensor/FileProcessor.kt
+++ src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/sensor/FileProcessor.kt
@@ -1,6 +1,6 @@
 package io.gitlab.arturbosch.detekt.sonar.sensor
 
-import io.gitlab.arturbosch.detekt.core.KtCompiler
+import io.gitlab.arturbosch.detekt.core.relativePath
 import io.gitlab.arturbosch.detekt.sonar.foundation.KotlinSyntax
 import org.jetbrains.kotlin.psi.KtFile
 import org.sonar.api.batch.sensor.SensorContext
@@ -15,8 +15,11 @@ class FileProcessor(private val context: SensorContext,
 	private val fileSystem = context.fileSystem()
 
 	fun run() {
+		val base = context.fileSystem().baseDir().toPath()
 		for (ktFile in ktFiles) {
-			fileSystem.inputFile { it.relativePath() == ktFile.getUserData(KtCompiler.RELATIVE_PATH) }?.let {
+			fileSystem.inputFile {
+				base.fileName.resolve(it.relativePath()).toString() == ktFile.relativePath()
+			}?.let {
 				fileStorage.save(ktFile, it)
 				KotlinSyntax.processFile(it, ktFile, context)
 			}
--- src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/sensor/ProjectMeasurementStorage.kt
+++ src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/sensor/ProjectMeasurementStorage.kt
@@ -1,11 +1,11 @@
 package io.gitlab.arturbosch.detekt.sonar.sensor
 
 import io.gitlab.arturbosch.detekt.api.Detektion
-import io.gitlab.arturbosch.detekt.core.processors.COMPLEXITY_KEY
-import io.gitlab.arturbosch.detekt.core.processors.LLOC_KEY
-import io.gitlab.arturbosch.detekt.core.processors.LOC_KEY
-import io.gitlab.arturbosch.detekt.core.processors.NUMBER_OF_COMMENT_LINES_KEY
-import io.gitlab.arturbosch.detekt.core.processors.SLOC_KEY
+import io.gitlab.arturbosch.detekt.core.processors.complexityKey
+import io.gitlab.arturbosch.detekt.core.processors.logicalLinesKey
+import io.gitlab.arturbosch.detekt.core.processors.linesKey
+import io.gitlab.arturbosch.detekt.core.processors.commentLinesKey
+import io.gitlab.arturbosch.detekt.core.processors.sourceLinesKey
 import org.jetbrains.kotlin.com.intellij.openapi.util.Key
 import org.sonar.api.batch.sensor.SensorContext
 import org.sonar.api.measures.Metric
@@ -14,11 +14,11 @@ class ProjectMeasurementStorage(private val detektion: Detektion,
 								private val context: SensorContext) {
 
 	fun run() {
-		save(LOC_KEY, LOC_PROJECT)
-		save(SLOC_KEY, SLOC_PROJECT)
-		save(LLOC_KEY, LLOC_PROJECT)
-		save(NUMBER_OF_COMMENT_LINES_KEY, CLOC_PROJECT)
-		save(COMPLEXITY_KEY, MCCABE_PROJECT)
+		save(linesKey, LOC_PROJECT)
+		save(sourceLinesKey, SLOC_PROJECT)
+		save(logicalLinesKey, LLOC_PROJECT)
+		save(commentLinesKey, CLOC_PROJECT)
+		save(complexityKey, MCCABE_PROJECT)
 	}
 
 	private fun save(dataKey: Key<Int>, metricKey: Metric<Int>) {
