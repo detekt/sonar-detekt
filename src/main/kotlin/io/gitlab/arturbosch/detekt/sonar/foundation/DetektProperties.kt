8374cb40a21ce0b3c7b52678fcd5b76b97f7f418
Artur Bosch <arturbosch@gmx.de>
2017-12-15 19:21:38 +0100

    Remove classpath config property
    This cannot be accessed by the user, so no need for that.

--- src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/foundation/DetektProperties.kt
+++ src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/foundation/DetektProperties.kt
@@ -12,19 +12,12 @@ const val CONFIG_PATH_DESCRIPTION = "Path to the detekt yaml config." +
 		" Path may be absolute or relative to the project base directory."
 const val CONFIG_PATH_DEFAULT = ""
 
-const val CONFIG_RESOURCE_KEY = "detekt.sonar.kotlin.config.resource"
-const val CONFIG_RESOURCE_DESCRIPTION = "Resource name of the detekt yaml config inside a custom rule set." +
-		" Only one of the configurations will be chosen: ConfigPath > ConfigResource."
-const val CONFIG_RESOURCE_DEFAULT = ""
-
 const val PATH_FILTERS_KEY = "detekt.sonar.kotlin.filters"
 const val PATH_FILTERS_DESCRIPTIONS = "Regex based path filters eg. '.*/test/.*'. " +
-		"All paths like '/my/custom/test/path' will be filtered. If no filters are specified" +
-		" defaults are: '.*/test/.*,.*/resources/.*,.*/build/.*,.*/target/.*'"
+		"All paths like '/my/custom/test/path' will be filtered."
 const val PATH_FILTERS_DEFAULTS = ".*/test/.*,.*/resources/.*,.*/build/.*,.*/target/.*"
 
-const val CONFIG_PATH_NAME = "Detekt yaml config path"
-const val CONFIG_RESOURCE_NAME = "Detekt yaml resource config"
+const val CONFIG_PATH_NAME = "Detekt yaml configuration path"
 const val PATH_FILTERS_NAME = "Detekt path filters"
 
 val propertyDefinitions = listOf<PropertyDefinition>(
@@ -32,12 +25,6 @@ val propertyDefinitions = listOf<PropertyDefinition>(
 				.name(CONFIG_PATH_NAME)
 				.defaultValue(CONFIG_PATH_DEFAULT)
 				.description(CONFIG_PATH_DESCRIPTION)
-				.onQualifiers(listOf(Qualifiers.PROJECT))
-				.build(),
-		PropertyDefinition.builder(CONFIG_RESOURCE_KEY)
-				.name(CONFIG_RESOURCE_NAME)
-				.defaultValue(CONFIG_RESOURCE_DEFAULT)
-				.description(CONFIG_RESOURCE_DESCRIPTION)
 				.onQualifiers(listOf(Qualifiers.PROJECT))
 				.build(),
 		PropertyDefinition.builder(PATH_FILTERS_KEY)
