b85349196c1aed67601c5bb054b2bc54ba45abfb
Shan Ul Haq <g@grevolution.me>
2018-07-15 09:57:07 +1000

    removed Deprecated ProfileDefinition and used BuiltInQualityProfilesDefinition

--- src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/profiles/KotlinProfile.kt
+++ src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/profiles/KotlinProfile.kt
@@ -4,25 +4,24 @@ import io.gitlab.arturbosch.detekt.sonar.foundation.DETEKT_WAY
 import io.gitlab.arturbosch.detekt.sonar.foundation.KEY
 import io.gitlab.arturbosch.detekt.sonar.rules.ruleKeys
 import io.gitlab.arturbosch.detekt.sonar.rules.severityTranslations
-import org.sonar.api.profiles.ProfileDefinition
-import org.sonar.api.profiles.RulesProfile
-import org.sonar.api.rules.Rule
-import org.sonar.api.rules.RulePriority
-import org.sonar.api.utils.ValidationMessages
+import org.sonar.api.server.profile.BuiltInQualityProfilesDefinition
 
 /**
  * @author Artur Bosch
  */
-class KotlinProfile : ProfileDefinition() {
+class KotlinProfile : BuiltInQualityProfilesDefinition {
+	override fun define(context: BuiltInQualityProfilesDefinition.Context?) {
+		context?.let {
+			val profile = context.createBuiltInQualityProfile(DETEKT_WAY, KEY)
+			profile.isDefault = true
 
-	override fun createProfile(validation: ValidationMessages): RulesProfile {
-		val profile = RulesProfile.create(DETEKT_WAY, KEY)
-		ruleKeys.filter { it.active }.forEach {
-			val severity = severityTranslations[it.issue.severity]
-			val priority = RulePriority.valueOfString(severity)
-			profile.activateRule(Rule.create(it.repository(), it.rule()), priority)
+			ruleKeys.filter { it.active }.forEach {
+				val severity = severityTranslations[it.issue.severity]
+				val rule = profile.activateRule(it.repository(), it.rule())
+				rule.overrideSeverity(severity)
+			}
+
+			profile.done()
 		}
-		return profile
 	}
-
 }
