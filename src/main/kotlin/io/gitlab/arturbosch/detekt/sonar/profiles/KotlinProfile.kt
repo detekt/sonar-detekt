e44bfc22c3e81735ed534fa58f6925dcdbb9294c
Artur Bosch <arturbosch@gmx.de>
2019-07-01 21:43:49 +0200

    Introduce new fail fast quality profile named 'detekt all'

--- src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/profiles/KotlinProfile.kt
+++ src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/profiles/KotlinProfile.kt
@@ -1,21 +1,35 @@
 package io.gitlab.arturbosch.detekt.sonar.profiles
 
+import io.gitlab.arturbosch.detekt.sonar.foundation.DETEKT_FAIL_FAST
 import io.gitlab.arturbosch.detekt.sonar.foundation.DETEKT_WAY
 import io.gitlab.arturbosch.detekt.sonar.foundation.KEY
+import io.gitlab.arturbosch.detekt.sonar.rules.DetektRuleKey
 import io.gitlab.arturbosch.detekt.sonar.rules.ruleKeys
 import io.gitlab.arturbosch.detekt.sonar.rules.severityTranslations
 import org.sonar.api.server.profile.BuiltInQualityProfilesDefinition
 
+typealias RuleContext = BuiltInQualityProfilesDefinition.Context
+
 class KotlinProfile : BuiltInQualityProfilesDefinition {
 
-    override fun define(context: BuiltInQualityProfilesDefinition.Context) {
-        val profile = context.createBuiltInQualityProfile(DETEKT_WAY, KEY)
-        profile.isDefault = true
+    override fun define(context: RuleContext) {
+        registerProfile(context, DETEKT_WAY, ruleKeys.filter { it.active }, isDefault = true)
+        registerProfile(context, DETEKT_FAIL_FAST, ruleKeys, isDefault = false)
+    }
 
-        ruleKeys.filter { it.active }.forEach {
-            val severity = severityTranslations[it.issue.severity]
-                ?: error("Unexpected severity '${it.issue.severity}'")
-            val rule = profile.activateRule(it.repository(), it.rule())
+    private fun registerProfile(
+        context: RuleContext,
+        name: String,
+        rules: List<DetektRuleKey>,
+        isDefault: Boolean
+    ) {
+        val profile = context.createBuiltInQualityProfile(name, KEY)
+        profile.isDefault = isDefault
+
+        for (ruleKey in rules) {
+            val severity = severityTranslations[ruleKey.issue.severity]
+                ?: error("Unexpected severity '${ruleKey.issue.severity}'")
+            val rule = profile.activateRule(ruleKey.repository(), ruleKey.rule())
             rule.overrideSeverity(severity)
         }
 
