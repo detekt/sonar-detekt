d25dbcf098b429131d9cc124a227b8aa744fa934
Jack Boswell <boswelja@outlook.com>
2023-06-07 07:28:12 +1200

    Update Detekt to v1.23.0 (#167)
    * Update Detekt
    
    * No wildcard import

--- src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/rules/DetektRuleKey.kt
+++ src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/rules/DetektRuleKey.kt
@@ -1,17 +1,20 @@
 package io.gitlab.arturbosch.detekt.sonar.rules
 
 import io.github.detekt.tooling.api.DefaultConfigurationProvider
+import io.github.detekt.tooling.dsl.ExtensionsSpecBuilder
 import io.gitlab.arturbosch.detekt.api.BaseRule
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.Issue
-import io.gitlab.arturbosch.detekt.api.MultiRule
 import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.RuleSetProvider
 import io.gitlab.arturbosch.detekt.sonar.foundation.REPOSITORY_KEY
 import org.sonar.api.rule.RuleKey
 import java.util.ServiceLoader
 
-internal val defaultConfig: Config = DefaultConfigurationProvider.load().get()
+internal val extensionSpec = ExtensionsSpecBuilder()
+    .build()
+
+internal val defaultConfig: Config = DefaultConfigurationProvider.load(extensionSpec).get()
 
 /**
  * Exclude similar or duplicated rule implementations from other rule sets than the default one.
@@ -29,7 +32,6 @@ internal val allLoadedRules: List<Rule> =
     ServiceLoader.load(RuleSetProvider::class.java, Config::class.java.classLoader)
         .asSequence()
         .flatMap { loadRules(it).asSequence() }
-        .flatMap { (it as? MultiRule)?.rules?.asSequence() ?: sequenceOf(it) }
         .filterIsInstance<Rule>()
         .filterNot { it.ruleId in excludedDuplicates }
         .toList()
